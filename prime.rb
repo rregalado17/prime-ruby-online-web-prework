e033978060c4486499579a0e9d8decb2d066a40d
magical-flow-3186 <rregalado17@gmail.com>
2019-06-04 03:58:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,4 @@
-def prime?(numbers)
-    numbers.each do |numbers|
-    i = 2
-      while i < num
-        if num % i == 0
-          puts "#{numbers} is not a prime"
-        else
-          return true
-        end
-        i += 1
-      end
-   end
+def prime?(n)
+      (2..Math.sqrt(n)).each { |i| return false if n % i == 0}
+      true
 end
