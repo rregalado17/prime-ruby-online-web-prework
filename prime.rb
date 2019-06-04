4d6e1363abaa5543563d96700ea32c832ed4d7f6
magical-flow-3186 <rregalado17@gmail.com>
2019-06-04 04:04:17 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,5 @@
 def prime?(n)
-      (2..Math.sqrt(n)).each { |i| return false if n % i == 0 || n < 2}
+      (2..Math.sqrt(n)).each { |i| return false if (n % i == 0) || (n < 2)}
       true
       
 end
